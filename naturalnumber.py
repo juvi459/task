5892da63c91d77d25b23e3bd1bd1b0a1734d4bf7
juvi459 <kabirjuveria@gmail.com>
2025-03-09 15:51:47 +0600

    Add files via upload

---
+++ naturalnumber.py
@@ -0,0 +1,10 @@
+num = int(input("Enter the number you wish to be checked as natural number "))
+
+if num>0:
+    print("Number is a Natural number")
+
+elif num==0:
+    print("Number is Natural")
+
+else:
+    print("Number is not Natural")    
